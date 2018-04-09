f53da688e782d0b749dbc309c1fc8b443a2eac1f
Bret Horak <bhorak@can-somebody-give-me-a-switch.local>
2018-04-09 10:55:22 -0500

    Created the puck and started making the lower paddle.

--- Xtreme Ice Hockey/GameScene.swift
+++ Xtreme Ice Hockey/GameScene.swift
@@ -11,79 +11,52 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var upperPaddle = SKSpriteNode()
+    var lowerPaddle = SKSpriteNode()
+    var puck = SKShapeNode()
     
-    override func didMove(to view: SKView) {
+    override func didMove(to view: SKView)
+    {
+        makePuck()
+        makeLowerPaddle()
+    }
+    
+    func makePuck()
+    {
+        puck = SKShapeNode(circleOfRadius: 50)
+        puck.position = CGPoint(x: frame.midX, y: frame.midY)
+        puck.strokeColor = UIColor.white
+        puck.fillColor = UIColor.black
+        puck.name = "puck"
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        // physics shape matches ball image
+        puck.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+        // ignores all forces and impluses
+        puck.physicsBody?.isDynamic = false
+        // use precise collision detection
+        puck.physicsBody?.usesPreciseCollisionDetection = true
+        // no loss os energy from friction
+        puck.physicsBody?.friction = 0
+        // gravity is not a factor
+        puck.physicsBody?.affectedByGravity = false
+        // bounces fully off of other objects
+        puck.physicsBody?.restitution = 1
+        // slows down over time
+        puck.physicsBody?.linearDamping = 1
+        puck.physicsBody?.contactTestBitMask = (puck.physicsBody?.collisionBitMask)!
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        addChild(puck) // add ball object to the view
+    }
+    
+    func makeLowerPaddle()
+    {
+        lowerPaddle = SKSpriteNode(color: UIColor.red, size: CGSize(width: frame.width/4, height: 50))
+        lowerPaddle.position = CGPoint(x: frame.midX, y: frame.minY + 125)
+        lowerPaddle.name = "lower paddle"
+        lowerPaddle.physicsBody = SKPhysicsBody(circleOfRadius: 70)
+        lowerPaddle.physicsBody?.isDynamic = false
+        addChild(lowerPaddle)
     }
     
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
 }
